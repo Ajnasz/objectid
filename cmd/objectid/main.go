07f97eb164897b74f77615b67ee8232a8e666d7f
Lajos Koszti <ajnasz@ajnasz.hu>
2023-06-13 07:55:36 +0200

    cli tool option to display objectid time value

--- cmd/objectid/main.go
+++ cmd/objectid/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/Ajnasz/objectid"
 )
@@ -10,7 +11,18 @@ import (
 func main() {
 	n := flag.Int("n", 1, "number of objectid to generate")
 	separator := flag.String("s", "\n", "separator between objectids")
+	toTime := flag.String("t", "", "convert objectid to time")
 	flag.Parse()
+
+	if toTime != nil && *toTime != "" {
+		oid, err := objectid.FromHex(*toTime)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println(oid.Time())
+		return
+	}
 
 	for i := 0; i < *n; i++ {
 		oid := objectid.New()
