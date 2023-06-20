dea1386bcf69a427e1ae4be8164a9bab97394ac3
Lajos Koszti <ajnasz@ajnasz.hu>
2023-06-20 22:56:37 +0200

    extend flags with base64 format and from date string

--- cmd/objectid/main.go
+++ cmd/objectid/main.go
@@ -17,17 +17,56 @@ func printTimeOfObjectID(idString string) {
 	fmt.Println(oid.Time())
 }
 
-func generate(n int, separator string) {
+func printHex(oid objectid.ObjectID, separator string) {
+	fmt.Printf("%s%s", oid.Hex(), separator)
+}
+
+func printBase64(oid objectid.ObjectID, separator string) {
+	fmt.Printf("%s%s", oid.Base64(), separator)
+}
+
+func generate(n int, separator string, format string) {
+	var printer func(objectid.ObjectID, string)
+	switch format {
+	case "hex":
+		printer = printHex
+	case "base64":
+		printer = printBase64
+	default:
+		printer = printHex
+	}
 	for i := 0; i < n; i++ {
 		oid := objectid.New()
-		fmt.Printf("%s%s", oid, separator)
+		printer(oid, separator)
 	}
+}
+
+func generateFromDate(date string, format string) {
+	var printer func(objectid.ObjectID, string)
+	switch format {
+	case "hex":
+		printer = printHex
+	case "base64":
+		printer = printBase64
+	default:
+		printer = printHex
+	}
+
+	oid, err := objectid.FromTime(date)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	printer(oid, "")
 }
 
 func main() {
 	n := flag.Int("n", 1, "number of objectid to generate")
-	separator := flag.String("s", "\n", "separator between objectids")
-	toTime := flag.String("t", "", "convert objectid to time")
+	separator := flag.String("separator", "\n", "separator between objectids")
+	format := flag.String("format", "hex", "format of objectid: hex, base64")
+	toTime := flag.String("to-time", "", "convert objectid to time")
+	fromTime := flag.String("from-time", "", "create a new objectid from a date time (RFC3339, $(date -I), $(date -Ihours), $(date -d -Iminutes), $(date -Iseconds)")
 	flag.Parse()
 
 	if toTime != nil && *toTime != "" {
@@ -35,5 +74,10 @@ func main() {
 		return
 	}
 
-	generate(*n, *separator)
+	if *fromTime != "" {
+		generateFromDate(*fromTime, *format)
+		return
+	}
+
+	generate(*n, *separator, *format)
 }
